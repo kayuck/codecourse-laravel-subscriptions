16b3e1a45d40514a8a290972b72329775b3297cd
flc-userU <s-uwazumi@fl-c.jp>
2022-08-09 16:22:08 +0900

    sec25

--- app/Http/Controllers/Subscriptions/SubscriptionController.php
+++ app/Http/Controllers/Subscriptions/SubscriptionController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Plan;
 use App\Rules\ValidCoupon;
 use Illuminate\Http\Request;
+use Laravel\Cashier\Exceptions\IncompletePayment;
 
 class SubscriptionController extends Controller
 {
@@ -32,9 +33,20 @@ class SubscriptionController extends Controller
 
         $plan = Plan::where('slug', $request->get('plan', 'monthly'))
             ->first();
-        $request->user()->newSubscription('default', $plan->stripe_id)
-            ->withCoupon($request->coupon)
-            ->create($request->token);
+
+        try {
+            $request->user()->newSubscription('default', $plan->stripe_id)
+                ->withCoupon($request->coupon)
+                ->create($request->token);
+        } catch (IncompletePayment $exception) {
+            return redirect()->route(
+                'cashier.payment',[
+                    $exception->payment->id,
+                    'redirect'=> route('account.subscriptions')
+                ]
+            );
+        }
+
 
         return back();
     }
